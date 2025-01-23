fef1e18ecd7108c75841b05968e4b459417f96be
oleh <olehlytvyn.personal@gmail.com>
2025-01-23 15:51:15 +0000

    Update shop models

--- demoshop/shop/models.py
+++ demoshop/shop/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    
     class Meta:
         ordering = ['name']
         indexes = [
@@ -12,8 +13,15 @@ class Category(models.Model):
         ]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+        
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_list_by_category', args=[self.slug]
+        )
+    
 class Product(models.Model):
     category = models.ForeignKey(
         Category,
@@ -40,3 +48,6 @@ class Product(models.Model):
         ]
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
