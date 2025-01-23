045512b35d54b9829f835b03d9a8d7791b6a50b7
oleh <olehlytvyn.personal@gmail.com>
2025-01-23 22:01:34 +0000

    Add shop - product detail / roduct list

--- demoshop/shop/views.py
+++ demoshop/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render # type: ignore
 
-
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 
@@ -22,11 +22,12 @@ def product_list(request, category_slug=None):
     )
     
 def product_detail(request, id, slug):
-    product = get_object_or_404( # type: ignore
+    product = get_object_or_404(
         Product, id=id, slug=slug, available=True
     )
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        {'product' : product}
+        {'product' : product, 'cart_product_form': cart_product_form}
     )
